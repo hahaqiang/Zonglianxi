b4ffce694899c5d0efa860c3fa339074cd3aa864
hahaqiang <493569992@qq.com>
2016-06-13 19:24:24 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/administrator/zonglianxi/MainActivity.java
+++ app/src/main/java/com/example/administrator/zonglianxi/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity{
 
     private NotificationManager manager;
-
+//SDFDSFDSFSDFSDFSDFSDFSDFDSFDSSSSSSSSSSSSSFSDFSDFDSF
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
